faf1effcfbbae4ab6234db49d90a537dbf6883ee
Riswan Ahamed <riswanahamed38&gmail.com>
2024-07-01 11:59:39 +0530

    Data Validation  Added

--- main.py
+++ main.py
@@ -2,37 +2,27 @@ from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.config.configuration import ConfigurationManager
 from src.mlProject.components.data_ingestion import DataIngestion
+from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
-
-STAGE_NAME = "Data Ingestion Pipeline"
-
-
-class DataIngestionTrainingPipeline:
-
-    def __init__(self):
-        pass
-    
-
-    def main(self):
-        config = ConfigurationManager()
-
-        data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config= data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
-
-
-if __name__ =="__main__":
-    try:
-        logger.info(STAGE_NAME)
-        data_ingestion_training_pipeline = DataIngestionTrainingPipeline()
-        data_ingestion_training_pipeline.main()
-        logger.info(STAGE_NAME + " completed successfully")
-    except Exception as e:
-        logger.error(STAGE_NAME + " failed")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
         logger.exception(e)
         raise e
 
 
 
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
